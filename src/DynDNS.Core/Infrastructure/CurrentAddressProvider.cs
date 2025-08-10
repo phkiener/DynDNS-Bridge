ac70240cde76c37da922392923c9a174fcfc0ec4
Philipp Kiener <kiener.phil@gmail.com>
2025-08-10 17:16:30 +0200

    Don't crash updating when IPv4 or IPv6 cannot be determined

--- src/DynDNS.Core/Infrastructure/CurrentAddressProvider.cs
+++ src/DynDNS.Core/Infrastructure/CurrentAddressProvider.cs
@@ -1,20 +1,38 @@
+using Microsoft.Extensions.Logging;
+
 namespace DynDNS.Core.Infrastructure;
 
-public sealed class CurrentAddressProvider(IHttpClientFactory httpClientFactory) : ICurrentAddressProvider
+public sealed class CurrentAddressProvider(IHttpClientFactory httpClientFactory, ILogger<CurrentAddressProvider> logger) : ICurrentAddressProvider
 {
     public async Task<string?> GetIPv4AddressAsync()
     {
-        var client = httpClientFactory.CreateClient();
-        var response = await client.GetAsync("https://4.icanhazip.com/");
+        try
+        {
+            var client = httpClientFactory.CreateClient();
+            var response = await client.GetAsync("https://4.icanhazip.com/");
 
-        return response.IsSuccessStatusCode ? (await response.Content.ReadAsStringAsync()).Trim() : null;
+            return response.IsSuccessStatusCode ? (await response.Content.ReadAsStringAsync()).Trim() : null;
+        }
+        catch (Exception e)
+        {
+            logger.LogError(e, "Failed to get IPv6 address.");
+            return null;
+        }
     }
 
     public async Task<string?> GetIPv6AddressAsync()
     {
-        var client = httpClientFactory.CreateClient();
-        var response = await client.GetAsync("https://6.icanhazip.com/");
+        try
+        {
+            var client = httpClientFactory.CreateClient();
+            var response = await client.GetAsync("https://6.icanhazip.com/");
 
-        return response.IsSuccessStatusCode ? (await response.Content.ReadAsStringAsync()).Trim() : null;
+            return response.IsSuccessStatusCode ? (await response.Content.ReadAsStringAsync()).Trim() : null;
+        }
+        catch (Exception e)
+        {
+            logger.LogError(e, "Failed to get IPv6 address.");
+            return null;
+        }
     }
 }
