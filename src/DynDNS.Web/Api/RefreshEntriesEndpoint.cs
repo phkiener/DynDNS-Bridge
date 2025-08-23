b3aaa294348a2b104008d00200656a5e79dac21b
Philipp Kiener <kiener.phil@gmail.com>
2025-08-23 12:31:09 +0200

    Use multi-update for endpoint

--- src/DynDNS.Web/Api/RefreshEntriesEndpoint.cs
+++ src/DynDNS.Web/Api/RefreshEntriesEndpoint.cs
@@ -6,19 +6,9 @@ namespace DynDNS.Web.Api;
 
 public static class RefreshEntriesEndpoint
 {
-    public static async Task HandleAsync(
-        HttpContext context,
-        [FromServices] IDomainBindings domainBindings,
-        [FromServices] ICurrentAddress currentAddress,
-        [FromServices] IProviderConfigurations providerConfigurations)
+    public static async Task HandleAsync(HttpContext context, [FromServices] IProviderConfigurations providerConfigurations)
     {
-        await currentAddress.RefreshAsync();
-
-        var bindings = await domainBindings.GetDomainBindingsAsync();
-        foreach (var binding in bindings.Where(static b => b.ConfiguredProvider is not null))
-        {
-            await providerConfigurations.UpdateBindingsAsync(binding.Id);
-        }
+        await providerConfigurations.UpdateAllBindingsAsync(context.RequestAborted);
 
         context.Response.StatusCode = StatusCodes.Status200OK;
         await context.Response.CompleteAsync();
